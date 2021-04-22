70c6155101a74657455ca2a01fd0a9a4501dd390
signeberden <si3087be-s@student.lu.se>
2021-04-22 23:01:41 +0200

    Second commit, buttons done and new activities created

--- app/src/main/java/com/example/compass/MainActivity.java
+++ app/src/main/java/com/example/compass/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.compass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /** Called when compass button is pressed*/
+    public void pressedCompass(View view){
+        Intent it = new Intent(this, CompassDisplay.class);
+        startActivity(it);
+    }
+    /**Called when accelerometer button is pressed*/
+    public void pressedAccelerometer(View view){
+        Intent intent = new Intent(this, AccelerometerDisplay.class);
+        startActivity(intent);
     }
 }
