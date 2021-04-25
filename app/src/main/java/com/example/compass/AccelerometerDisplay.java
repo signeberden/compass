67d9696ae8dc7b4755164cf55c25c9ac34fc28f1
signeberden <si3087be-s@student.lu.se>
2021-04-25 21:15:41 +0200

    Working app with compass and accelorometer

--- app/src/main/java/com/example/compass/AccelerometerDisplay.java
+++ app/src/main/java/com/example/compass/AccelerometerDisplay.java
@@ -3,12 +3,99 @@ package com.example.compass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
 
-public class AccelerometerDisplay extends AppCompatActivity {
+public class AccelerometerDisplay extends AppCompatActivity implements SensorEventListener {
+
+    private TextView xText;
+    private TextView yText;
+    private TextView zText;
+    private TextView maxX;
+    private TextView maxY;
+    private TextView maxZ;
+    private TextView direction;
+    private Sensor mySensor;
+    private SensorManager SM;
+
+    private float deltaXMax = 0;
+    private float deltaYMax = 0;
+    private float deltaZMax = 0;
+
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+
+    private float lastX, lastY, lastZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer_display);
+
+        //Creating Sensor Manager
+        SM= (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        //Accelerometer sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Register sensor listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Assign text view
+        xText = (TextView)findViewById(R.id.currentX);
+        yText = (TextView)findViewById(R.id.currentY);
+        zText = (TextView)findViewById(R.id.currentZ);
+        maxX = (TextView) findViewById(R.id.maxX);
+        maxY = (TextView) findViewById(R.id.maxY);
+        maxZ = (TextView) findViewById(R.id.maxZ);
+        direction = (TextView) findViewById(R.id.textView3);
+
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xText.setText("" + event.values[0]);
+        yText.setText("" + event.values[1]);
+        zText.setText("" + event.values[2]);
+
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+        displayMaxValues();
+        float x = event.values[0];
+
+        if(x<-1){
+            direction.setText("Right");
+        } else if (x>1){
+            direction.setText("Left");
+        } else{
+            direction.setText("");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //Not in use
+    }
+    // display the max x,y,z accelerometer values
+    public void displayMaxValues() {
+        if (deltaX > deltaXMax) {
+            deltaXMax = deltaX;
+            maxX.setText(Float.toString(deltaXMax));
+        }
+        if (deltaY > deltaYMax) {
+            deltaYMax = deltaY;
+            maxY.setText(Float.toString(deltaYMax));
+        }
+        if (deltaZ > deltaZMax) {
+            deltaZMax = deltaZ;
+            maxZ.setText(Float.toString(deltaZMax));
+        }
     }
 }
--- app/src/main/java/com/example/compass/CompassDisplay.java
+++ app/src/main/java/com/example/compass/CompassDisplay.java
@@ -1,14 +1,170 @@
 package com.example.compass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class CompassDisplay extends AppCompatActivity {
+public class CompassDisplay extends AppCompatActivity implements SensorEventListener {
+
+    ImageView compass_img;
+    TextView txt_compass;
+    int mAzimuth;
+    private SensorManager mSensorManager;
+    private Sensor mRotationV, mAccelerometer, mMagnetometer;
+    boolean haveSensor = false, haveSensor2 = false;
+    float[] rMat = new float[9];
+    float[] orientation = new float[3];
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    Vibrator vibrator;
+
+    static final float ALPHA = 0.25f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass_display);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        compass_img = (ImageView) findViewById(R.id.compass_image);
+        txt_compass = (TextView) findViewById(R.id.DegreeTV);
+        start();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rMat, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(rMat, orientation);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        mAzimuth = Math.round(mAzimuth);
+        compass_img.setRotation(-mAzimuth);
+
+        String where = "NW";
+
+        if (mAzimuth >= 350 || mAzimuth <= 10) {
+            where = "NORTH";
+            txt_compass.setTextColor(Color.rgb(150, 195, 120));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                vibrator.vibrate(500);
+            }
+
+        }
+        if (mAzimuth < 350 && mAzimuth > 280)
+            where = "NORTH WEST";
+        if (mAzimuth <= 280 && mAzimuth > 260)
+            where = "WEST";
+        if (mAzimuth <= 260 && mAzimuth > 190)
+            where = "SOUTH WEST";
+        if (mAzimuth <= 190 && mAzimuth > 170)
+            where = "SOUTH";
+        if (mAzimuth <= 170 && mAzimuth > 100)
+            where = "SOUTH EAST";
+        if (mAzimuth <= 100 && mAzimuth > 80)
+            where = "EAST";
+        if (mAzimuth <= 80 && mAzimuth > 10)
+            where = "NORTH EAST";
+
+
+        txt_compass.setText(mAzimuth + "° " + where);
+
+
+    }
+    /** Low pass filtering*/
+    protected float[] lowPass(float[] input, float[] output ) {
+        if ( output == null ) return input;
+        for ( int i=0; i<input.length; i++ ) {
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+        }
+        return output;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public void start() {
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) {
+            if ((mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) || (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)) {
+                noSensorsAlert();
+            }
+            else {
+                mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+                haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+                haveSensor2 = mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
+            }
+        }
+        else{
+            mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    public void noSensorsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("Your device doesn't support the Compass.")
+                .setCancelable(false)
+                .setNegativeButton("Close",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    public void stop() {
+        if (haveSensor) {
+            mSensorManager.unregisterListener(this, mRotationV);
+        }
+        else {
+            mSensorManager.unregisterListener(this, mAccelerometer);
+            mSensorManager.unregisterListener(this, mMagnetometer);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        start();
     }
 }
